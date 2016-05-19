bbcb3608d0813d48abe9294a0b385f7067dbd3bd
19mariusz95 <19mariusz95@gmail.com>
2016-05-19 12:46:02 +0200

    server bind

--- zad1/server.c
+++ zad1/server.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <zconf.h>
+
+int inet_socket;
+int unix_socket;
+char *path;
+long port;
+
+
+struct sockaddr_un unix_address;
+struct sockaddr_in inet_address;
+struct sockaddr_un client_unix_address;
+struct sockaddr_in client_inet_address;
+
+void sig_handler(int sig) {
+    exit(0);
+}
+
+void cleanup() {
+    close(inet_socket);
+    close(unix_socket);
+    remove(path);
+}
 
 int main(int argc, char *argv[]) {
 
@@ -7,8 +33,39 @@ int main(int argc, char *argv[]) {
         printf("no enough args\n");
         exit(-1);
     }
-    long port = strtol(argv[1], NULL, 10);
-    char *path = argv[2];
+    atexit(cleanup);
+    signal(SIGINT, sig_handler);
+    port = strtol(argv[1], NULL, 10);
+    path = argv[2];
+
+    unix_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+    inet_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (unix_socket == -1 || inet_socket == -1) {
+        printf("socket error\n");
+        exit(-7);
+    }
+
+    memset(&unix_address, 0, sizeof(unix_address));
+    memset(&inet_address, 0, sizeof(inet_address));
+    unix_address.sun_family = AF_UNIX;
+    strcpy(unix_address.sun_path, path);
+
+    inet_address.sin_family = AF_INET;
+    inet_address.sin_port = htons((uint16_t) port);
+    inet_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(unix_socket, (struct sockaddr *) &unix_address, sizeof(unix_address)) != 0) {
+        perror(NULL);
+        exit(-5);
+    }
+    if (bind(inet_socket, (struct sockaddr *) &inet_address, sizeof(inet_address)) != 0) {
+        perror(NULL);
+        exit(-6);
+    }
+
+
+
     return 0;
 }
 
---
+++ zad1/trzustka.h
@@ -0,0 +1,15 @@
+#ifndef ZAD1_TRZUSTKA_H
+#define ZAD1_TRZUSTKA_H
+
+#define MAX_CLIENTS 100
+#define MAX_MESSAGE_LENGTH 256
+#define MAX_CLIENT_NAME_LENGTH 32
+#define MAX_RESPONSE_TIME 5
+
+
+typedef struct {
+    char name[MAX_CLIENT_NAME_LENGTH];
+
+} client;
+
+#endif //ZAD1_TRZUSTKA_H

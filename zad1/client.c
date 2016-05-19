30b2e30d6afea5275e15e7367ccb6403383bb33b
19mariusz95 <19mariusz95@gmail.com>
2016-05-19 12:01:49 +0200

    read args

--- zad1/client.c
+++ zad1/client.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
+    if (argc < 4) {
+        printf("no enough args\n");
+        exit(-1);
+    }
+    char *client_id = argv[1];
+    long type = strtol(argv[2], NULL, 10);
+    if (type == 0) { // local
+        char *path = argv[3];
+    } else if (type == 1) {
+        if (argc < 5) {
+            printf("no enough args\n");
+            exit(-1);
+        }
+        char *ip = argv[3];
+        long port = strtol(argv[4], NULL, 10);
+        printf("%s %ld %s %ld\n", client_id, type, ip, port);
 
-    printf("Hello World!!\n");
+    } else {
+        printf("bad arg\n");
+        exit(-2);
+    }
     return 0;
 }
--- zad1/server.c
+++ zad1/server.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
-    printf("Hello World!!\n");
+    if (argc < 3) {
+        printf("no enough args\n");
+        exit(-1);
+    }
+    long port = strtol(argv[1], NULL, 10);
+    char *path = argv[2];
     return 0;
 }
 
